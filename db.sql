e13953f2dec726f97429b4831d101cb73ce9ae31
joseferalvarez <joseferalvarezromero@gmail.com>
2024-08-26 18:13:44 +0200

    user table added to script

--- db.sql
+++ db.sql
@@ -1,3 +1,20 @@
-DROP DATABASE IF EXISTS devspace;
-CREATE DATABASE devspace;
+CREATE DATABASE IF NOT EXISTS devspace;
 USE devspace;
+
+CREATE TABLE IF NOT EXISTS profiles (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  profile_id int NOT NULL AUTO_INCREMENT,
+  username varchar(50),
+  slug varchar(100),
+  fullname varchar(100),
+  position varchar(200),
+  description varchar(2000),
+  social_media varchar(50),
+  photo varchar(200),
+  cv varchar(200),
+  preferred_technologies varchar(50),
+  skills varchar(50),
+  phone varchar(9),
+  email varchar(100),
+  language varchar(2),
+);
